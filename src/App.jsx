fcf0dac818c7662c995cb850a857725b41f1ab79
Vika <125195456+VikaSukhara@users.noreply.github.com>
2025-07-25 13:40:21 +0200

    add header and theme switch button, switch to yarn from npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Header from "./header/header";
 
 function App() {
+  const [isDark, setIsDark] = useState(() => {
+    const theme = localStorage.getItem("theme");
+    return theme === "dark" ? true : false;
+  });
+
+  useEffect(() => {
+    if (isDark) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDark]);
+
   return (
-    <>
+    <div>
       <h1>Task Dashboard</h1>
-    </>
+      <Header changeTheme={setIsDark} isDark={isDark} />
+    </div>
   );
 }
 
