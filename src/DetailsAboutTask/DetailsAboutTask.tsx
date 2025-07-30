48d18f4b9aaea0f8eec56afda175693b24174dd4
Vika <125195456+VikaSukhara@users.noreply.github.com>
2025-07-30 16:39:39 +0200

    added deletion of task

--- src/DetailsAboutTask/DetailsAboutTask.tsx
+++ src/DetailsAboutTask/DetailsAboutTask.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { taskType } from "../App";
 import styles from "./DetailsAboutTask.module.css";
 type Props = {
   task: taskType;
+  onDelete: (id: string) => void;
 };
-export default function DetailsAboutTask({ task }: Props) {
+
+import { Trash } from "lucide-react";
+import Modal from "../Modal/Modal";
+
+export default function DetailsAboutTask({ task, onDelete }: Props) {
+  const [isDeleteConfirmOpen, setIsDeleteConfirmOpen] =
+    useState<boolean>(false);
+
   const formatDate = (date: Date) => {
     const day = String(date.getDate()).padStart(2, "0");
     const month = String(date.getMonth() + 1).padStart(2, "0");
@@ -55,6 +64,41 @@ export default function DetailsAboutTask({ task }: Props) {
           <span className={styles.span}>Created at:</span>{" "}
           {formatDate(new Date(task.createdAt))}
         </p>
+      )}
+
+      <button
+        className={styles.btnTrash}
+        onClick={() => setIsDeleteConfirmOpen(true)}
+      >
+        <Trash className={styles.icon} />
+      </button>
+
+      {isDeleteConfirmOpen && (
+        <Modal
+          isOpen={isDeleteConfirmOpen}
+          onClose={() => setIsDeleteConfirmOpen(false)}
+        >
+          <div className={styles.modalContainer}>
+            <h3>Ви хочете видалити завдання?</h3>
+            <div className={styles.btnConfirmationWrapper}>
+              <button
+                className={styles.modalBtn}
+                onClick={() => {
+                  onDelete(task.id);
+                  setIsDeleteConfirmOpen(false);
+                }}
+              >
+                Так
+              </button>
+              <button
+                className={styles.modalBtn}
+                onClick={() => setIsDeleteConfirmOpen(false)}
+              >
+                Ні
+              </button>
+            </div>
+          </div>
+        </Modal>
       )}
     </div>
   );
