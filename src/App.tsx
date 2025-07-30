1fad9353507c56886f62084d62c17569772dbfb2
Vika <125195456+VikaSukhara@users.noreply.github.com>
2025-07-30 17:12:44 +0200

    added notification after task creation and localStororage

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Header from "./header/Header";
 import TaskContainers from "./TaskContainers/TaskContainers";
 import styles from "./App.module.css";
 import { v4 as uuidv4 } from "uuid";
+import { Toaster } from "react-hot-toast";
+import toast from "react-hot-toast";
 
 export type taskType = {
   id: string;
@@ -22,16 +24,34 @@ function App() {
     const theme = localStorage.getItem("theme");
     return theme === "dark" ? true : false;
   });
+  const [tasks, setTasks] = useState<taskType[]>(() => {
+    const saved = localStorage.getItem("tasks");
+    return saved ? JSON.parse(saved) : [];
+  });
 
-  const [tasks, setTasks] = useState<taskType[]>([]);
+  const [notifications, setNotifications] = useState<
+    { id: string; message: string; date: string }[]
+  >(() => {
+    const saved = localStorage.getItem("notifications");
+    return saved ? JSON.parse(saved) : [];
+  });
 
-  const addTask = (newTask: taskType) => {
-    console.log("addTask");
-    console.log("addTask in", tasks);
-    setTasks([...tasks, newTask]);
-  };
+  const [hasUnread, setHasUnread] = useState(false);
 
-  console.log("addTask out", tasks);
+  useEffect(() => {
+    if (isSidebarOpen) {
+      setHasUnread(false);
+    }
+  }, [isSidebarOpen]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    localStorage.setItem("notifications", JSON.stringify(notifications));
+  }, [notifications]);
+
   useEffect(() => {
     if (isDark) {
       document.documentElement.classList.add("dark");
@@ -41,6 +61,49 @@ function App() {
       localStorage.setItem("theme", "light");
     }
   }, [isDark]);
+
+  const addNotification = (message: string) => {
+    const newNotification = {
+      id: uuidv4(),
+      message,
+      date: new Date().toLocaleString(),
+    };
+    setNotifications((prev) => [newNotification, ...prev]);
+    setHasUnread(true);
+    toast.success(
+      <>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h5 style={{ fontSize: "18px", fontWeight: "600" }}>
+            New Notification:
+          </h5>
+
+          <div>
+            <p style={{ fontSize: "16px", fontWeight: "400" }}>
+              Task created successfully! "{message}"
+            </p>
+          </div>
+        </div>
+      </>,
+      {
+        style: {
+          background: "#3b82f6",
+          color: "#fff",
+          padding: "8px",
+          borderRadius: "6px",
+          fontSize: "14px",
+          whiteSpace: "pre-line",
+        },
+        icon: null,
+        duration: 5000,
+      }
+    );
+  };
+
+  const addTask = (newTask: taskType) => {
+    console.log("addTask");
+    console.log("addTask in", tasks);
+    setTasks([...tasks, newTask]);
+  };
 
   return (
     <div className={styles.appWrapper}>
@@ -53,6 +116,8 @@ function App() {
           isSidebarOpen ? styles.shifted : ""
         }`}
         addTask={addTask}
+        addNotification={addNotification}
+        hasUnread={hasUnread}
       />
       <main
         className={`${styles.main} ${
@@ -71,8 +136,21 @@ function App() {
           <h3 className={styles.sidebarTitle}>Notifications</h3>
         </div>
 
-        <p className={styles.sidebarPar}>No notifications.</p>
+        {notifications ? (
+          <ul>
+            {notifications.map((n) => (
+              <li key={n.id} className={styles.notificationsList}>
+                <p className={styles.notificationsTitle}>{n.message}</p>
+
+                <p className={styles.notificationsData}>{n.date}</p>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p className={styles.sidebarPar}>No notifications.</p>
+        )}
       </aside>
+      <Toaster position="top-right" />
     </div>
   );
 }
